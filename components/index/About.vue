d3dbc0d6fd076ad8f540f24e8b70b5d944eee676
MatrixMe <67325727+MatrixMe00@users.noreply.github.com>
2023-01-30 21:34:03 +0000

    Update to about and hero sections
    Added real content to various components

--- components/index/About.vue
+++ components/index/About.vue
@@ -2,10 +2,13 @@
     <section v-if="page == 'home'" class="py-12 px-8 bg-gradient-to-br from-blue-900 via-blue-700 to-blue-500 flex flex-wrap items-center text-white
         xl:container xl:mx-auto"
     >
-        <div class="left text-center py-6 px-4 md:py-auto md:px-auto w-full md:w-3/5">
+        <div class="left text-center py-6 px-4 md:py-auto md:px-auto w-full md:w-3/5 space-y-2 lg:space-y-6">
             <h1 class="text-4xl font-bold">About Us</h1>
             <div class="content text-lg">
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam maxime necessitatibus molestiae fuga consectetur odio non inventore eaque voluptatibus. Veniam vero nesciunt autem cupiditate. Quia exercitationem impedit nobis alias a.</p>
+                <p>
+                    Purity Conference is a conference organized, especially for the youth, to preach and impart purity into the generation. The aim of our conferences is to impart each individual with the desire to remain pure for the Christ.
+                    Though it might not look it, but there is so much the future suffers if the current generation is not cautioned on the relevance of being pure.
+                </p>
             </div>
         </div>
         <div class="right h-72 bg-cover bg-center bg-gray-300 w-full md:w-2/5" style="background-image: url('/assets/img/img1.jpg')"></div>
@@ -17,7 +20,7 @@
         <div class="text-center py-6 px-4 md:py-auto md:px-auto self-center w-full md:w-3/5">
             <h1 class="text-4xl font-bold">About Founder</h1>
             <div class="content text-base mt-4 md:text-lg">
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam maxime necessitatibus molestiae fuga consectetur odio non inventore eaque voluptatibus. Veniam vero nesciunt autem cupiditate. Quia exercitationem impedit nobis alias a.</p>
+                <p>Description not provided yet: Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam maxime necessitatibus molestiae fuga consectetur odio non inventore eaque voluptatibus. Veniam vero nesciunt autem cupiditate. Quia exercitationem impedit nobis alias a.</p>
             </div>
         </div>
         <div class="h-72 bg-cover bg-center bg-gray-300 w-full md:w-2/5" style="background-image: url('/assets/img/img1.jpg')"></div>
--- components/index/Hero.vue
+++ components/index/Hero.vue
@@ -2,7 +2,7 @@
     <section class="m-4 xl:container xl:mx-auto">
         <div class="lg:mx-12 border shadow-md py-12 px-7 flex flex-col sm:justify-between md:flex-row justify-center items-center space-y-4 md:space-y-0 md:space-x-16">
             <p class="font-semibold text-2xl md:text-4xl">Grab Your Ticket</p>
-            <button class="border w-1/3 py-4 px-3 border-blue-500 hover:bg-blue-500 hover:text-white sm:w-1/6 text-lg md:text-xl" onclick="document.getElementById('ticketButton').click()">Get a Ticket</button>
+            <button class="border w-full md:w-1/3 lg:w-1/4 xl:w-1/5 py-4 px-3 border-blue-500 hover:bg-blue-500 hover:text-white sm:w-1/6 text-lg md:text-xl" onclick="document.getElementById('ticketButton').click()">Get a Ticket</button>
         </div>
     </section>
 
@@ -10,7 +10,7 @@
         xl:container xl:mx-auto"
     >
         <h1 class="text-4xl font-bold text-center text-white pb-4">Our Speakers</h1>
-        <div class="heros grid gap-2 md:gap-3 lg:gap-4 md:grid-cols-2 lg:grid-cols-3 content-center justify-center py-8">
+        <div class="heros grid gap-2 md:gap-3 lg:gap-4 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 content-center justify-center py-8">
             <!--Hero cards-->
             <Card v-for="(c, index) in heros" 
                 :key="index" 
@@ -22,11 +22,20 @@
 </template>
 
 <script setup>
-    const heros = ref([
-        {title: "Speaker Name", content: "Purity Conference 2021"},
-        {title: "Speaker Name", content: "Purity Conference 2022"},
-        {title: "Speaker Name", content: "Purity Conference 2022"}
-    ])
+    import { guests } from '~~/composables/photos';
+
+    const online = true;
+    const heros = ref([])
+    
+    if(online){
+        heros.value = guests
+    }else{
+        heros.value = [
+            {title: "Speaker Name", content: "Purity Conference 2021"},
+            {title: "Speaker Name", content: "Purity Conference 2022"},
+            {title: "Speaker Name", content: "Purity Conference 2022"}
+        ]
+    }
 </script>
 
 <style scoped>
